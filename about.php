e89f90e95f59607a77eee86477bcbb45c81c8c4d
SudharsanSandyy007 <73164927+SudharsanSandyy007@users.noreply.github.com>
2021-12-10 09:44:01 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,79 @@
+<?php
+
+//if (session_status() !== PHP_SESSION_ACTIVE) {session_start();} for php 5.4 and above
+
+if(session_id() == '' || !isset($_SESSION)){session_start();}
+
+
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>About Us || PRODUCT MANAGEMENT SYSTEM </title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+
+    <nav class="top-bar" data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+          <h1><a href="index.php">PRODUCT MANAGEMENT SYSTEM</a></h1>
+        </li>
+        <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+      </ul>
+
+      <section class="top-bar-section">
+      <!-- Right Nav Section -->
+        <ul class="right">
+          <li class="active"><a href="about.php">About</a></li>
+          <li><a href="products.php">Products</a></li>
+          <li><a href="cart.php">View Cart</a></li>
+          <li><a href="orders.php">My Orders</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          <?php
+    
+          if(isset($_SESSION['username'])){
+            echo '<li><a href="account.php">My Account</a></li>';
+            echo '<li><a href="logout.php">Log Out</a></li>';
+          }
+          else{
+            echo '<li><a href="login.php">Log In</a></li>';
+            echo '<li><a href="register.php">Register</a></li>';
+          }
+          ?>
+        </ul>
+      </section>
+    </nav>
+
+
+
+
+    <div class="row" style="margin-top:30px;">
+      <div class="small-12">
+        <p>PRODUCT MANAGEMENT SYSTEM is a project on E-Commerce Website for buying electronic appliances online. The Images/Icons used here are not owned by us. And this is used for educational Purpose.
+        <p>Why PRODUCT MANAGEMENT SYSTEM? We had a vision of opening an e-commerce store exclusively for exclusively for Electronic Applainces but we have'nt got enough time to explore the cloud technology enough. And lastly, we got to experience and learn cloud through the course through this semester and had also got a change to get in hands on experience with Cloud. So went for it.</p>
+
+        <footer>
+           <p style="text-align:center; font-size:0.8em;">&copy;PRODUCT MANAGEMENT SYSTEM. All Rights Reserved.</p>
+        </footer>
+
+      </div>
+    </div>
+
+
+
+
+
+
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
---
+++ inventorymanagement.sql
@@ -0,0 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 10, 2021 at 04:56 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inventorymanagement`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(15) NOT NULL,
+  `product_code` varchar(255) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_desc` varchar(255) NOT NULL,
+  `price` int(10) NOT NULL,
+  `units` int(5) NOT NULL,
+  `total` int(15) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `email` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `product_code`, `product_name`, `product_desc`, `price`, `units`, `total`, `date`, `email`) VALUES
+(12, 'PRODUCT3', 'MORIC color usb 2.0 flash drive', 'NEW 2.0 Moric TF&usb pendrive 16gb metal usb flash drive flash ... kebidu Bluetooth 5.0 Receiver Car Kit MP3 Player Decoder Board Color ', 1000, 1, 1000, '2021-11-19 09:57:53', 'admin@admin.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `product_code` varchar(60) NOT NULL,
+  `product_name` varchar(60) NOT NULL,
+  `product_desc` tinytext NOT NULL,
+  `product_img_name` varchar(60) NOT NULL,
+  `qty` int(5) NOT NULL,
+  `price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `product_code`, `product_name`, `product_desc`, `product_img_name`, `qty`, `price`) VALUES
+(1, 'PRODUCT1', 'APPLE Waltch Series 3', 'APPLE Watch Series 3 GPS - MTEY2HN/A 38 mm Silver Aluminium Case with Fog Sport Band  (White Strap, Regular) ', 'iwatch.jpeg', 20, '19900.00'),
+(2, 'PRODUCT2', 'ASUS Zenbook 13', 'ASUS Zenbook 13 Intel Core i5-1135G7 11th Gen 13.3 inches OLED FHD OLED, Business Laptop (8GB/512GB SSD/Office 2019/Windows 10 Home/Iris Xe Graphics/Grey/1.14 Kg), UX325EA-KG502TS', 'asuszenbook.jpg', 110, '74990.00'),
+(3, 'PRODUCT3', 'MORIC color usb 2.0 flash drive', 'NEW 2.0 Moric TF&usb pendrive 16gb metal usb flash drive flash ... kebidu Bluetooth 5.0 Receiver Car Kit MP3 Player Decoder Board Color ', 'pendrive.jpg', 33, '1000.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `pin` int(6) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `type` varchar(20) NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fname`, `lname`, `address`, `city`, `pin`, `email`, `password`, `type`) VALUES
+(2, 'Admin', 'Webmaster', 'Internet', 'Electricity', 101010, 'admin@admin.com', 'admin', 'admin'),
+(5, 'Vignesh ', 'P', 'skdjfsd', 'jfksdjf', 2390432, 'vignesh.p2019@vitstudent.ac.in', '123456', 'user');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `product_code` (`product_code`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
